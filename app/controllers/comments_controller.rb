4b6655d26a43a0290602d027ae848915883b0baa
David Karpay <dlkarpay@gmail.com>
2013-11-28 22:39:54 -0500

    what is wrong?

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,7 @@
 class CommentsController < ApplicationController
-  def index
-  end
-
   def create
-    @post = Post.find(params[:post_id])#first find the post upon which to comment
-    @comment = #now, how to generate the comment's model
-@post.comments.create(params[:comment].permit(:commenter, :body))
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
     redirect_to post_path(@post)
   end
 end
